f8a2a81ee965a6d092b9ae012c9cc99fc5c59533
deamacto <pszon.joanna@gmail.com>
2023-10-18 10:36:22 +0200

    added Item

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import io.jenetics.internal.util.Requires;
+
+import java.util.random.RandomGenerator;
+
+public record Item(double size, double value) {
+    public Item {
+        Requires.nonNegative(size);
+        Requires.nonNegative(value);
+    }
+
+    public static Item random(final RandomGenerator random) {
+        return new Item(
+                random.nextDouble() * 100,
+                random.nextDouble() * 100
+        );
+    }
+}
